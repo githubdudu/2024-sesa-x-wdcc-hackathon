cc42071b9d846aadc05fb9d32d648dd1ba09a00f
hdon694 <hdon694@aucklanduni.ac.nz>
2024-08-03 00:37:10 +1200

    Setting a basic page for deploying and start.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+import ufo from './UFO.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={ufo} className="team-logo" alt="team-logo" />
+        <p>We are We ARE TEAM POKEMON TRAINS.</p>
+        <p>We are going to build something amazing with React!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.notion.so/dewey-dong/2024-SESA-x-WDCC-Hackathon-7351b114ef9d41d89896215db729428d"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Our Notion Page
         </a>
       </header>
     </div>
