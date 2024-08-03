a72cde876b1cb6151d709812fea2266ff59c0bfd
hdon694 <hdon694@aucklanduni.ac.nz>
2024-08-03 13:19:48 +1200

    Added tailwind CSS support. Changed traditional CSS to Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="text-center">
+      <header
+        className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white"
+        style={{ fontSize: 'calc(10px + 2vmin)' }}
+      >
         <img src={ufo} className="team-logo" alt="team-logo" />
-        <p>We are We ARE TEAM POKEMON TRAINS.</p>
-        <p>We are going to build something amazing with React!</p>
+
+        <h1 className="text-3xl font-bold underline">TEAM POKEMON TRAINERS</h1>
+        <p> We are going to build something amazing with React!</p>
         <a
-          className="App-link"
+          className="text-blue-300 underline hover:text-blue-900"
           href="https://www.notion.so/dewey-dong/2024-SESA-x-WDCC-Hackathon-7351b114ef9d41d89896215db729428d"
           target="_blank"
           rel="noopener noreferrer"
